d7f71b702c366bf7829d35ed4015d66ccb869cda
Admin <Admin@DESKTOP-JUGL5UQ>
2026-04-27 06:23:38 -0700

    Narayan 1st commit

---
+++ mavencommandProject/src/test/java/mavencommandProject/contactTest.java
@@ -0,0 +1,16 @@
+package mavencommandProject;
+
+import org.testng.annotations.Test;
+
+public class contactTest {
+
+	@Test
+	public void m1Test() {
+		System.out.println("hii Test1===");
+	}
+	
+	@Test
+	public void m2Test() {
+		System.out.println(" hi test 2");
+	}
+}
---
+++ mavencommandProject/src/test/java/packagae2/mavenTest.java
@@ -0,0 +1,16 @@
+package packagae2;
+
+import org.testng.annotations.Test;
+
+public class mavenTest {
+
+	@Test
+	public void n1Test() {
+		System.out.println("hii Test1===");
+	}
+	
+	@Test
+	public void n2Test() {
+		System.out.println(" hi test 2");
+	}
+}
